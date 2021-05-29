84571ad0ada4c5e33f7ec44ade805fdcaa0e3235
jylee <stpn94@gmail.com>
2021-05-30 01:09:36 +0900

    이미지파일 생성을 위한 DTO 추가

---
+++ src/main/java/proj21_shop/dto/product/ImageDTO.java
@@ -0,0 +1,20 @@
+package proj21_shop.dto.product;
+
+public class ImageDTO {
+	// 이미지 파일 생성을 위한 DTO
+
+	/* 경로 */
+	private String uploadPath;
+
+	/* uuid */
+	private String uuid;
+
+	/* 파일 이름 */
+	private String fileName;
+
+	/* 상품 num */
+	private int pro_num;
+
+	/* 메인이미지 여부 */
+	private int pro_img_state;
+}
