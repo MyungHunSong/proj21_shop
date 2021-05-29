fb9b2c4861060ef7896718aca2c43dc3d3098e34
jylee <stpn94@gmail.com>
2021-05-30 01:08:11 +0900

    Q&A DTO 추가

---
+++ src/main/java/proj21_shop/dto/qna/QnaDTO.java
@@ -0,0 +1,17 @@
+package proj21_shop.dto.qna;
+
+import java.sql.Date;
+
+public class QnaDTO {
+	private int q_index;
+	private String q_title;
+	private String q_option;
+	private String q_member;
+	private String q_content;
+	private String q_file;
+	private Date q_date;
+	private int q_hit;
+	private int q_group;
+	private int q_step;
+	private int q_indent;
+}
