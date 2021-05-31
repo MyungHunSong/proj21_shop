80fc0b02bdf00892520115625123a54ffff1c2d4
nyLee <skdus2421@naver.com>
2021-05-31 16:52:36 +0900

    로그인 폼 작성

--- src/main/java/proj21_shop/config/ContextRoot.java
+++ src/main/java/proj21_shop/config/ContextRoot.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({ContextDataSource.class, ContextSqlSession.class, MvcConfig.class})
 @ComponentScan(basePackages = {
-	    "proj21_shop.config.service"
-	  , "proj21_shop.config.controller"
+	    "proj21_shop.service"
+	  , "proj21_shop.controller"
 		})
 public class ContextRoot {
 	
