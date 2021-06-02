37909ace4512741d88fc9514ce8a4bad306fbdb3
jylee <stpn94@gmail.com>
2021-06-02 09:26:11 +0900

    MvcConfig 수정

--- src/main/java/proj21_shop/config/ContextRoot.java
+++ src/main/java/proj21_shop/config/ContextRoot.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ContextDataSource.class, ContextSqlSession.class, MvcConfig.class, TilesConfig.class})
+@Import({ContextDataSource.class, ContextSqlSession.class, MvcConfig.class})
 @ComponentScan(basePackages = {
 	    "proj21_shop.service"
 	  , "proj21_shop.controller"
