6aded2747c1c239fc633cb2fff3b0d1df72883d4
jylee <stpn94@gmail.com>
2021-06-02 14:11:55 +0900

    수량 출력 Mapper 추가(8개)

--- Scripts/4.test_jongyoon.sql
+++ Scripts/4.test_jongyoon.sql
@@ -20,13 +20,45 @@ select *
   from re_reply;
 select *
   from review;
+ 
 -- 만약 주문 수 가 0이면 false 아니면 true
 select if(count(*)=0,'false','true')
   from `order`;
 
+select *
+  from `order`;
+ 
+-- 총 판매 금액
+select sum(order_pro_quantity * order_price)
+  from `order`;
+ 
+-- 총 주문 건 수
+select count(distinct order_num)
+  from `order`;
 
--- 총 평균
 -- 총 주문 수
+select count(*)
+  from `order`;
+-- 배송 준비 중 수 
+select count(*)
+  from `order`
+ where delivery_status='배송준비중';
+-- 배송 중 수 
+select count(*)
+  from `order`
+ where delivery_status='배송 중';
+-- 배송 완료 수
+select count(*)
+  from `order`
+ where delivery_status='배송 완료';
+-- 환불 완료 수
+select count(*)
+  from `order` 
+ where delivery_status='환불 완료';
+-- 환불 대기 수
+select count(*)
+  from `ORDER`
+ where delivery_status='환불 대기중';
 -- 총 주문
 -- 모든 환불완료
 
--- src/test/java/proj21_shop/mapper/admin/order/AdminOrderMapperTest.java
+++ src/test/java/proj21_shop/mapper/admin/order/AdminOrderMapperTest.java
@@ -1,7 +1,5 @@
 package proj21_shop.mapper.admin.order;
 
-import static org.junit.Assert.fail;
-
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 import org.junit.After;
@@ -38,6 +36,65 @@ public class AdminOrderMapperTest {
 		String res=mapper.atLeastOneOrder();
 		Assert.assertEquals("true", res);
 		log.debug("주문이 있나? >>" + res);
+	}
+	@Test
+	public void test2GetTotalAvenue() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.getTotalAvenue();
+		log.debug("총 판매 금액? >>" + res);
+
+	}
+
+	@Test
+	public void test3GetTotalOrderCount() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.getTotalOrderCount();
+		log.debug("총 주문 건 수?" + res);
+
+	}
+
+	@Test
+	public void test4SelectTotalOrders() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.selectTotalOrders();
+		log.debug("총 주문 수? >>" + res);
+
+	}
+	
+	@Test
+	public void test5SelectAllWaitingDelivery() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.selectAllWaitingDelivery();
+		log.debug("배송준비중? >>" + res);
+
+	}
+	
+	@Test
+	public void test6SelectAllOnDelivery() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.selectAllOnDelivery();
+		log.debug("배송 중? >>" + res);
+	}
+	
+	@Test
+	public void test7SelectAllTotDoneDelivery() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.selectAllTotDoneDelivery();
+		log.debug("배송 완료? >>" + res);
+	}
+	
+	@Test
+	public void test8SelectAllDoneRefund() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.selectAllDoneRefund();
+		log.debug("환불 완료? >>" + res);
+	}
+	
+	@Test
+	public void test9SelectAllWaitingRefund() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		int res=mapper.selectAllWaitingRefund();
+		log.debug("환불 대기중? >>" + res);
 	}
 
 }
