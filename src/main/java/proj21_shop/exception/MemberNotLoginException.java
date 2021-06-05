eda19e7fa61fe457f06d722f66cb8c391072f8a6
taehoon95 <leeth950304@gmail.com>
2021-06-05 15:26:37 +0900

    로그인 한 경우만 서비스 이용할수 있게 안하면 오류 생성

---
+++ src/main/java/proj21_shop/exception/MemberNotLoginException.java
@@ -0,0 +1,14 @@
+package proj21_shop.exception;
+
+@SuppressWarnings("serial")
+public class MemberNotLoginException extends RuntimeException {
+
+	
+	public MemberNotLoginException() {
+	}
+
+	public MemberNotLoginException(String arg0) {
+		super(arg0);
+	}
+	
+}
