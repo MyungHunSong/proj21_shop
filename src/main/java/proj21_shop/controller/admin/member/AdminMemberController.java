07d263d98f5cc310756c70f773b2420bb6e299e2
jylee <stpn94@gmail.com>
2021-06-07 21:03:21 +0900

    adminMember 작업

---
+++ src/main/java/proj21_shop/controller/admin/member/AdminMemberController.java
@@ -0,0 +1,26 @@
+package proj21_shop.controller.admin.member;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import proj21_shop.service.admin.member.AdminMemberService;
+
+@Controller("adminMemberController")
+@RequestMapping("/admin/member/")
+public class AdminMemberController {
+	@Autowired
+	AdminMemberService adminMemberService;
+	
+	@RequestMapping("listMembers")
+	public ModelAndView searchMember(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/admin/order/listMembers");
+		System.out.println(mav);
+		return mav;
+	}
+}
---
+++ src/main/java/proj21_shop/mapper/admin/member/AdminMemberMapper.java
@@ -0,0 +1,26 @@
+package proj21_shop.mapper.admin.member;
+
+import java.util.List;
+import java.util.Map;
+
+import proj21_shop.dto.member.MemberDTO;
+
+public interface AdminMemberMapper {
+	
+	/*
+	 * void deleteMember(String memberId);
+	 * 
+	 * MemberDTO selectMemberByCertain(Map searchMap);
+	 */
+
+	List<MemberDTO> selectAllMembersList(Map pagingMap);//
+	
+	int selectTotalMembers();//
+
+	int selectAllMemberMen();//
+
+	int selectAllMemberWomen();//
+
+	int selectedMembers(Map<String, Object> pagingMap);//
+
+}
