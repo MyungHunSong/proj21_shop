a2831d7cc505567c8f19822e8a6ec5ce6b44bb1a
jylee <stpn94@gmail.com>
2021-06-23 20:52:41 +0900

    common error 페이지 생성

--- src/main/java/proj21_shop/interceptor/AdminCheckInterceptor.java
+++ src/main/java/proj21_shop/interceptor/AdminCheckInterceptor.java
@@ -6,22 +6,23 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import proj21_shop.dto.member.AuthInfo;
 import proj21_shop.dto.member.MemberDTO;
 
 public class AdminCheckInterceptor extends HandlerInterceptorAdapter {
 
-	 @Override
-	 public boolean preHandle(HttpServletRequest req,
-	    HttpServletResponse res, Object obj) throws Exception {
-	  
-	  HttpSession session = req.getSession();
-	  MemberDTO user = (MemberDTO)session.getAttribute("authInfo");
-	  
-	  if(user == null || user.getMemberId() != "admin") {
-	   res.sendRedirect(req.getContextPath() + "/login");
-	   return false;
-	  }
-	  
-	  return true;
-	 }
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object obj) throws Exception {
+
+		HttpSession session = req.getSession();
+		if (session != null) {
+			AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+			
+			if (authInfo == null || authInfo.getId().equals("admin")) {
+				return true;
+			}
+		}
+		res.sendRedirect(req.getContextPath() + "/main");
+		return false;
+	}
 }
